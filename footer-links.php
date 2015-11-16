9ef7fc2de007ef3743967cb4ffa242feb896348b
Dan Phiffer <dan@phiffer.org>
2015-11-16 14:34:53 -0500

    🔧 New footer links page template

---
+++ footer-links.php
@@ -0,0 +1,11 @@
+Find me on
+<?php
+
+wp_list_bookmarks(array(
+	'title_li' => '',
+	'orderby' => 'rating',
+	'order' => 'DESC',
+	'categorize' => false
+));
+
+?>
--- footer.php
+++ footer.php
@@ -1,17 +1,6 @@
 		<footer class="dark">
 			<div class="links">
-				Find me on
-				<?php
-				
-				wp_list_bookmarks(array(
-					'title_li' => '',
-					'orderby' => 'rating',
-					'order' => 'DESC',
-					'categorize' => false
-				));
-				
-				?>
-				</ul>
+				<?php get_template_part('footer-links'); ?>
 			</div>
 			<p class="website-credit">
 				Website by <a href="http://phiffer.org/">Dan Phiffer</a> /
