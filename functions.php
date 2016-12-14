7a763a5ba8c052ecbc564dd0fd2c25bb6bbdeaa0
Dan Phiffer <dan@phiffer.org>
2016-12-14 12:28:10 -0500

    :wrench: fix indents

--- functions.php
+++ functions.php
@@ -30,10 +30,10 @@ function hackery_enqueue_scripts() {
 add_action( 'wp_enqueue_scripts', 'hackery_enqueue_scripts' );
 
 function hackery_setup_front_name() {
-  $page_on_front =      get_option( 'page_on_front' );
+ 	$page_on_front =      get_option( 'page_on_front' );
 	$page_on_front_name = get_option( 'page_on_front_name' );
 	if ( ! empty( $page_on_front ) &&   empty( $page_on_front_name ) ||
-		     empty( $page_on_front ) && ! empty( $page_on_front_name )) {
+	       empty( $page_on_front ) && ! empty( $page_on_front_name )) {
 		hackery_update_page_on_front( $page_on_front );
 	}
 }
@@ -60,8 +60,8 @@ function hackery_save_post( $postid, $post ) {
 	if ( $postid == $page_on_front ) {
 		update_option( 'page_on_front_name', $post->post_name );
 	}
-  hackery_add_front_page_rewrites( $page_on_front );
-  $wp_rewrite->flush_rules( false );
+	hackery_add_front_page_rewrites( $page_on_front );
+	$wp_rewrite->flush_rules( false );
 }
 add_action( 'save_post', 'hackery_save_post', 10, 2 );
 
@@ -95,15 +95,15 @@ function hackery_add_front_page_rewrites( $post_parent, $path = '' ) {
 
 function hackery_page_link( $url, $post ) {
 	$stem = get_option( 'page_on_front_name' );
-  if ( ! empty( $stem ) ) {
-  	$url = preg_replace( '/^(https?:\/\/[^\/]+)\/' . $stem . '/', '$1', $url );
-  }
-  return $url;
+	if ( ! empty( $stem ) ) {
+		$url = preg_replace( '/^(https?:\/\/[^\/]+)\/' . $stem . '/', '$1', $url );
+	}
+	return $url;
 }
 add_filter( 'page_link', 'hackery_page_link', 10, 2 );
 
 function hackery_modified( $file ) {
-  return date( 'YmdHis', filemtime( get_template_directory() . $file ) );
+	return date( 'YmdHis', filemtime( get_template_directory() . $file ) );
 }
 
 function hackery_wp_head() {
@@ -115,15 +115,15 @@ function hackery_wp_footer() {
 }
 
 function hackery_wp_markup( $function ) {
-  ob_start();
-  $function();
-  $markup = ob_get_contents();
-  ob_end_clean();
-  $markup = preg_replace( '#^<#m', "\t\t<", $markup );
-  $markup = preg_replace( '# />#m', '>', $markup );
-  $markup = str_replace( "'", '"', $markup );
-  $markup = ltrim( $markup );
-  return $markup;
+	ob_start();
+	$function();
+	$markup = ob_get_contents();
+	ob_end_clean();
+	$markup = preg_replace( '#^<#m', "\t\t<", $markup );
+	$markup = preg_replace( '# />#m', '>', $markup );
+	$markup = str_replace( "'", '"', $markup );
+	$markup = ltrim( $markup );
+	return $markup;
 }
 
 function hackery_header() {
@@ -141,10 +141,10 @@ function hackery_header() {
 }
 
 function hackery_the_post() {
-  global $_hackery_template_depth;
-  if ( $_hackery_template_depth == 1 ) {
-  	the_post();
-  }
+	global $_hackery_template_depth;
+	if ( $_hackery_template_depth == 1 ) {
+		the_post();
+	}
 }
 
 function hackery_footer() {
@@ -178,12 +178,12 @@ function hackery_menu_links() {
 }
 
 function hackery_data_image() {
-  $image_id = get_post_thumbnail_id();
-  if (empty($image_id)) {
-  	return '';
-  }
-  list($src) = wp_get_attachment_image_src($image_id, 'full');
-  echo " data-background-image=\"$src\"";
+	$image_id = get_post_thumbnail_id();
+	if (empty($image_id)) {
+		return '';
+	}
+	list($src) = wp_get_attachment_image_src($image_id, 'full');
+	echo " data-background-image=\"$src\"";
 }
 
 function hackery_page_class( $page_class = '' ) {
@@ -191,8 +191,8 @@ function hackery_page_class( $page_class = '' ) {
 	$page_class = str_replace('.php', '', basename( $page_template ) );
 	
 	$page_format = get_post_format();
-  if ( ! empty( $page_format ) ) {
-  	$page_class .= " format-$page_format";
+	if ( ! empty( $page_format ) ) {
+		$page_class .= " format-$page_format";
 	}
 	
 	$meta_page_class = get_post_meta( get_the_ID(), 'page_class', true );
@@ -204,33 +204,33 @@ function hackery_page_class( $page_class = '' ) {
 
 function hackery_body_class( $body_class ) {
 	$page_template = get_page_template();
-  if ( 'page-stack.php' != basename( $page_template ) ) {
-  	$hackery_page_class = hackery_page_class();
-  	$hackery_classes = explode( ' ', trim( $hackery_page_class ) );
-  	$body_class = array_merge( $body_class, $hackery_classes );
-  }
-  return $body_class;
+	if ( 'page-stack.php' != basename( $page_template ) ) {
+		$hackery_page_class = hackery_page_class();
+		$hackery_classes = explode( ' ', trim( $hackery_page_class ) );
+		$body_class = array_merge( $body_class, $hackery_classes );
+	}
+	return $body_class;
 }
 add_filter( 'body_class', 'hackery_body_class' );
 
 function hackery_page_attributes() {
 	global $post;
 	$page_id = hackery_path_id( get_permalink() );
-  echo " id=\"page-$page_id\"";
-  $page_class = hackery_page_class();
-  $page_class = apply_filters( 'hackery_page_class', $page_class, $post );
-  if ( 'image' === get_post_format() ) {
-  	hackery_data_image();
-  }
-  if ( ! empty( $page_class ) ) {
-  	$page_class = esc_attr($page_class);
-  	echo " class=\"$page_class\"";
-  }
+	echo " id=\"page-$page_id\"";
+	$page_class = hackery_page_class();
+	$page_class = apply_filters( 'hackery_page_class', $page_class, $post );
+	if ( 'image' === get_post_format() ) {
+		hackery_data_image();
+	}
+	if ( ! empty( $page_class ) ) {
+		$page_class = esc_attr($page_class);
+		echo " class=\"$page_class\"";
+	}
 }
 
 function hackery_path_id( $url ) {
-  $url = parse_url( $url );
-  return $url['path'];
+	$url = parse_url( $url );
+	return $url['path'];
 }
 
 function hackery_title() {
@@ -251,8 +251,8 @@ function hackery_title() {
 	$title = str_replace( ' ', '&nbsp;', $title );
 	$url = get_permalink();
 	$tag = ( $post->post_parent == 0 ) ? 'h1' : 'h2';
-  array_unshift( $breadcrumbs, "<$tag><a href=\"$url\">$title</a></$tag>" );
-  echo '<div class="breadcrumbs">' . implode( ' / ', $breadcrumbs ) . "</div>\n";
+	array_unshift( $breadcrumbs, "<$tag><a href=\"$url\">$title</a></$tag>" );
+	echo '<div class="breadcrumbs">' . implode( ' / ', $breadcrumbs ) . "</div>\n";
 }
 
 function hackery_gallery_shortcode( $attr ) {
@@ -264,7 +264,7 @@ remove_shortcode( 'gallery', 'gallery_shortcode' );
 add_shortcode( 'gallery', 'hackery_gallery_shortcode' );
 
 function hackery_gallery_link_caption( $html ) {
-  $doc = hackery_dom_document( $html );
+	$doc = hackery_dom_document( $html );
 	$figures = $doc->getElementsByTagName( 'figure' );
 	foreach ( $figures as $figure ) {
 		$imgs = $figure->getElementsByTagName( 'img' );
@@ -374,17 +374,17 @@ function hackery_get_image_tag( $html, $id, $alt, $title, $align, $size ) {
 	}
 	$width =  " width=\"$width\"";
 	$height = " height=\"$height\"";
-  $alt =    ' alt="' . esc_attr( $alt ) . '"';
-  $class =  " class=\"size-$size\"";
-  return "<img$src$width$height$alt$class>";
+	$alt =    ' alt="' . esc_attr( $alt ) . '"';
+	$class =  " class=\"size-$size\"";
+	return "<img$src$width$height$alt$class>";
 }
 add_filter( 'get_image_tag', 'hackery_get_image_tag', 10, 6 );
 
 function hackery_gallery_content( $html ) {
 	global $post;
-  if ( 'gallery' !== get_post_format() ) {
-  	return $html;
-  }
+	if ( 'gallery' !== get_post_format() ) {
+		return $html;
+	}
 	$children = get_posts( array(
 		'post_parent' => $post->ID,
 		'post_type' => 'page',
@@ -407,33 +407,33 @@ add_filter( 'the_content', 'hackery_gallery_content', 0 );
 
 function hackery_cleanup_content( $html ) {
 	$doc = hackery_dom_document( $html );
-  $figures = $doc->getElementsByTagName( 'figure' );
-  foreach ( $figures as $figure ) {
-  	if ( strtolower( $figure->parentNode->nodeName ) == 'p' ) {
-  		$figure->parentNode->parentNode->insertBefore( $figure, $figure->parentNode );
-  	}
-  }
-  $iframes = $doc->getElementsByTagName( 'iframe' );
-  foreach ( $iframes as $iframe ) {
-  	$iframe->removeAttribute( 'frameborder' );
-  	$iframe->removeAttribute( 'webkitallowfullscreen' );
-  	$iframe->removeAttribute( 'mozallowfullscreen' );
-  	if ( strtolower( $iframe->parentNode->nodeName ) == 'p' ) {
-  		$parent = $iframe->parentNode;
-  		$grandparent = $parent->parentNode;
-  		$grandparent->insertBefore( $iframe, $parent );
-  		$grandparent->removeChild( $parent );
-  	}
-  }
-  $html = hackery_document_html( $doc );
-  $html = str_replace('</iframe><br>', '</iframe>', $html);
-  
-  return $html;
+	$figures = $doc->getElementsByTagName( 'figure' );
+	foreach ( $figures as $figure ) {
+		if ( strtolower( $figure->parentNode->nodeName ) == 'p' ) {
+			$figure->parentNode->parentNode->insertBefore( $figure, $figure->parentNode );
+		}
+	}
+	$iframes = $doc->getElementsByTagName( 'iframe' );
+	foreach ( $iframes as $iframe ) {
+		$iframe->removeAttribute( 'frameborder' );
+		$iframe->removeAttribute( 'webkitallowfullscreen' );
+		$iframe->removeAttribute( 'mozallowfullscreen' );
+		if ( strtolower( $iframe->parentNode->nodeName ) == 'p' ) {
+			$parent = $iframe->parentNode;
+			$grandparent = $parent->parentNode;
+			$grandparent->insertBefore( $iframe, $parent );
+			$grandparent->removeChild( $parent );
+		}
+	}
+	$html = hackery_document_html( $doc );
+	$html = str_replace('</iframe><br>', '</iframe>', $html);
+
+	return $html;
 }
 add_filter( 'the_content', 'hackery_cleanup_content', 100 );
 
 function hackery_dom_document( $html ) {
-  libxml_use_internal_errors(true);
+	libxml_use_internal_errors(true);
 	$doc = new DOMDocument();
 	$options = 0;
 	if ( defined( 'LIBXML_HTML_NOIMPLIED' ) ) {
@@ -447,12 +447,12 @@ function hackery_dom_document( $html ) {
 }
 
 function hackery_document_html( $doc ) {
-  $html = $doc->saveHTML();
-  $html = str_replace( "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n", '', $html );
+	$html = $doc->saveHTML();
+	$html = str_replace( "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n", '', $html );
 	$html = str_replace( "<?xml encoding=\"UTF-8\"?>", '', $html );
-  $html = str_replace( '<html><body>', '', $html );
+	$html = str_replace( '<html><body>', '', $html );
 	$html = str_replace( '</body></html>', '', $html );
-  return $html;
+	return $html;
 }
 
 function hackery_normalize_urls( $content ) {
